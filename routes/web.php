468bc4110bbc7a057f405e7453406315528b7fc4
Emilien Cuny <emilien.cuny0312@gmail.com>
2023-12-04 18:29:59 +0100

    register/login/profile

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', function () { 
-    return view('auth.register');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
-
-Route::post('/store', [LoginRegisterController::class,'store']);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
