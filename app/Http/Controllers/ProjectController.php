51fb4d98e125644435cf67a1d395b522af461837
Emilien <emilien.cuny0312@gmail.com>
2024-01-17 23:13:58 +0100

    [Refactor] get code widely cleaner + validators

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\TeamJoinRequest;
 use App\Models\TeamUser;
+use Faker\Factory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
@@ -11,11 +14,16 @@ use App\Models\Team;
 use App\Models\User;
 use App\Models\Status;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\View\View;
+use Mockery\Exception;
 
 class ProjectController extends Controller
 {
 
-    private function isImage(Request $request) {
+    private function isImage(Request $request): string|null
+    {
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $image = $request->file('image');
 
@@ -26,94 +34,184 @@ class ProjectController extends Controller
             $image->storeAs('projects/images', $imageName, 'public');
 
             return $imageName;
+        } else {
+            return null;
         }
     }
 
     /**
-     * Create a new project
+     * Create a project
+     *
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function create(Request $request) {
+    public function create(Request $request): RedirectResponse
+    {
+        $validator = Validator::make($request->all(), Project::$rules, Project::$messages);
 
-        $image = $this->isImage($request);
-
-        // Store project in database
-        $project = Project::create([
-            'title' => $request->title,
-            'description' => $request->description,
-            'image' => (isset($image)) ? 'storage/projects/images/' . $image : "storage/projects/images/default.jpg",
-            'owner_id' => Auth::id(),
-            'status_id' => $request->status
-        ]);
-
-        Team::create([
-            'name' => $request->title,
-            'personal_team' => 1,
-            'user_id' => Auth::id(),
-            'project_id' => $project->id,
-        ]);
-
-        return to_route('project.create.post');
-    }
-
-    public function delete(Request $request) {
-        if ($request->owner_id == Auth::id()) {
-            Project::destroy($request->id);
-            Team::where('project_id', $request->id)->delete();
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
         }
-        return to_route('home');
+
+        try {
+            $image = $this->isImage($request);
+            $project = Project::create([
+                'title' => $request->title,
+                'description' => $request->description,
+                'image' => (isset($image)) ? 'storage/projects/images/' . $image : "storage/projects/images/default.jpg",
+                'owner_id' => Auth::id(),
+                'status_id' => $request->status
+            ]);
+
+            Team::create([
+                'name' => $request->title,
+                'personal_team' => 1,
+                'user_id' => Auth::id(),
+                'project_id' => $project->id,
+            ]);
+
+            return to_route('project.create.post');
+        } catch (\Exception $e) {
+            Log::error("Impossible de créer le projet ou l'équipe : " . $e->getMessage());
+            return redirect()->back()->withErrors("Impossible de créer le projet ou l'équipe.");
+        }
+
     }
 
-    public function show(int $id) {
-        $project = Project::find($id);
-        $team = Team::where('project_id', $id)->first();
-        $owner_project = User::where('id', $team->user_id)->first();
-        $users_id = TeamUser::where('team_id', $team->id)->pluck('user_id')->toArray();
-        $users_belongs_project = User::whereIn('id', $users_id)->get();
-
-        $isAlreadyJoinRequest = TeamJoinRequest::where('user_id', Auth::id())->where('team_id', $team->id)->first();
-
-        return view('project.show',[
-            'project' => $project,
-            'users' => $users_belongs_project,
-            'owner' => $owner_project,
-            'team' => $team,
-            'isAlreadyJoinRequest' => $isAlreadyJoinRequest,
-        ]);
+    /**
+     * Delete a project
+     *
+     * @param int $projectId
+     * @return RedirectResponse
+     */
+    public function delete(int $projectId): RedirectResponse
+    {
+        try {
+            $project = Project::findOrFail($projectId);
+            if ($project->owner_id == Auth::id()) {
+                Project::destroy($projectId);
+                Team::where('project_id', $projectId)->delete();
+            }
+            return to_route('home');
+        } catch (ModelNotFoundException $e) {
+            Log::error("Le projet à supprimer n'a pas été trouvé : " . $e->getMessage());
+            return abort(404);
+        } catch (\Exception $e) {
+            Log::error("Impossible de supprimer le projet :" . $e->getMessage());
+            return redirect()->back()->withErrors("Impossible de supprimer le projet.");
+        }
     }
 
-    public function showAll() {
-        $projects = Project::all();
 
-        return view('welcome', [
-            'projects' => $projects
-        ]);
+    /**
+     * Retrieve a unique project
+     *
+     * @param int $id
+     * @return View|RedirectResponse
+     */
+    public function show(int $id): View|RedirectResponse
+    {
+        try {
+            $project = Project::find($id);
+            $team = Team::where('project_id', $id)->firstOrFail();
+            $owner_project = User::where('id', $team->user_id)->firstOrFail();
+            $users_id = TeamUser::where('team_id', $team->id)->pluck('user_id')->toArray();
+            $users_belongs_project = User::whereIn('id', $users_id)->get();
+
+            $isAlreadyJoinRequest = TeamJoinRequest::where('user_id', Auth::id())->where('team_id', $team->id)->first();
+
+            return view('project.show',[
+                'project' => $project,
+                'users' => $users_belongs_project,
+                'owner' => $owner_project,
+                'team' => $team,
+                'isAlreadyJoinRequest' => $isAlreadyJoinRequest,
+            ]);
+        } catch (ModelNotFoundException $e) {
+            Log::error("Impossible de récupérer les informations du projet.");
+            return redirect()->back()->withErrors("Impossible de récupérer les informations du projet.");
+        } catch (\Exception $e) {
+            Log::error("Une erreur s'est produite lors de la récupération de projet : " . $e->getMessage());
+            return redirect()->back()->withErrors("Une erreur s'est produite.");
+        }
     }
 
-    public function update(Request $request) {
-        $project = Project::find($request->id);
+    /**
+     * Retrieve all projects
+     *
+     * @return View|RedirectResponse
+     */
+    public function index(): View|RedirectResponse
+    {
+        try {
+            $projects = Project::all();
 
-        $image = $this->isImage($request);
-
-        $statuses = Status::find($project->id);
-
-        $project->update([
-            'title' => $request->title,
-            'description' => $request->description,
-            'image' => (isset($image)) ? 'storage/projects/images/' . $image : $project->image,
-            'status_id' => $request->status
-        ]);
-
-        return to_route('home');
+            return view('welcome', [
+                'projects' => $projects
+            ]);
+        } catch (\Exception $e) {
+            Log::error("Impossible de charger les projets : " . $e->getMessage());
+            return redirect()->back()->withErrors("Impossible de récupérer les projets.");
+        }
     }
 
-    public function updateForm(int $id) {
-        $project = Project::find($id);
+    /**
+     * Update the project
+     *
+     * @param Request $request
+     * @param int $projectId
+     * @return RedirectResponse
+     */
+    public function update(Request $request, int $projectId): RedirectResponse
+    {
+        $validator = Validator::make($request->all(), Project::$rules, Project::$messages);
 
-        $statuses = Status::all();
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
 
-        return view('project.update', [
-            'project' => $project,
-            'statuses' => $statuses,
-        ]);
+        try {
+            $project = Project::findOrFail($projectId);
+            $image = $this->isImage($request);
+
+            $project->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'image' => (isset($image)) ? 'storage/projects/images/' . $image : $project->image,
+                'status_id' => $request->status
+            ]);
+
+            return to_route('home');
+        } catch (ModelNotFoundException $e) {
+            Log::error("Impossible de trouver le projet à modifer :" . $e->getMessage());
+            return abort(404);
+        } catch (\Exception $e) {
+            Log::error('Impossible de modifier le projet : ' . $e->getMessage());
+            return redirect()->back()->withErrors("Impossible de mofier le projet.");
+        }
+    }
+
+    /**
+     *  Redirect to update form
+     *
+     * @param int $id
+     * @return View|RedirectResponse
+     */
+    public function updateForm(int $id): View|RedirectResponse
+    {
+        try {
+            $project = Project::findOrFail($id);
+            $statuses = Status::all();
+
+            return view('project.update', [
+                'project' => $project,
+                'statuses' => $statuses,
+            ]);
+        } catch (ModelNotFoundException $e) {
+            Log::error('Le projet n\'a pas été trouvé : ' . $e->getMessage());
+            return abort(404);
+        } catch (\Exception) {
+            return redirect()->back()->withErrors(['message' => "Une erreur est survenue lors de la récupération de données."]);
+        }
     }
 }
