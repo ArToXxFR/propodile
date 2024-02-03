0333e84a25faf4018ed2989d652fbe80024dbced
Emilien <emilien.cuny0312@gmail.com>
2024-02-03 09:38:40 +0100

    [Fix] page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,12 @@
             @foreach ($projects as $project)
                 <div class="w-full sm:w-1/2 lg:w-1/4 p-2">
                     <a href="{{ route("project.show", ["id" => $project->id]) }}" class="block rounded border overflow-hidden hover:shadow-lg transition duration-300 ease-in-out">
-                        <div class="p-4">
+                        <div class="p-4 flex flex-col items-center">
                             <h3 class="text-xl font-semibold text-indigo-600 mb-2 hover:text-indigo-800">{{ $project->title }}</h3>
                             <p class="text-gray-700 mb-2 h-20 overflow-hidden line-clamp-4">{{ $project->description }}</p>
-                            <img src="{{ asset("storage/projects/images/" . basename($project->image)) }}" alt="" class="w-full h-auto rounded-lg mb-2">
+                            <div class="flex justify-center items-center mb-2">
+                                <img src="{{ asset("storage/projects/images/" . basename($project->image)) }}" alt="" class="rounded-lg max-w-full max-h-48">
+                            </div>
                             <p class="text-gray-600 font-bold">Chef de projet: {{ $project->owner->username }}</p>
                         </div>
                     </a>
